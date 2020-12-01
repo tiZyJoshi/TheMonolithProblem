bb5be649f8dc00bdf2b40b3b81a4ada54361b71a
Johannes Poehn <johannes.poehn@gmail.com>
2020-12-01 14:44:45 +0100

    Initial commit

---
+++ Assets/DataLoader.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Xml.Serialization;
+using UnityEngine;
+
+public class DataLoader : MonoBehaviour
+{
+    public GameObject ServicePrefab;
+
+    public int ClusterCount = 10;
+    public int Iterations = 100;
+
+    private List<Service> Services;
+    private List<ServiceContainer> ServiceContainer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        var serializer = new XmlSerializer(typeof(List<Service>));
+        using (var reader = new FileStream("Assets/data.xml", FileMode.Open))
+        {
+            Services = serializer.Deserialize(reader) as List<Service>;
+        }
+        if (Services is null)
+        {
+            throw new InvalidOperationException();
+        }
+
+        var side = (int)Math.Ceiling(Math.Sqrt(Services.Count));
+
+        ServiceContainer = Services
+            .Select((_, index) => Instantiate(ServicePrefab, new Vector3(index/side, index%side), Quaternion.identity))
+            .Select(go => go.GetComponent<ServiceContainer>())
+            .ToList();
+
+        
+
+        for (var i = 0; i < Services.Count; i++)
+        {
+            ServiceContainer[i].Init(Services[i], ServiceContainer);
+        }
+    }
+
+    // Update is called once per frame
+    private bool running = true;
+    private float timeScaleBackup;
+    private bool clusterized = false;
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (running)
+            {
+                timeScaleBackup = Time.timeScale;
+                Time.timeScale = 0;
+                running = false;
+            }
+            else
+            {
+                Time.timeScale = timeScaleBackup;
+                running = true;
+                clusterized = false;
+            }
+        }
+
+        if (running || clusterized) return;
+
+        var items = ServiceContainer.Select(c => c.transform.position).ToArray();
+        var result = KMeans.Cluster(items, ClusterCount, Iterations, 0);
+
+        Gizmos.color = Color.yellow;
+        for (var i = 0; i < result.clusters.Length; i++)
+        {
+            var color = Color.HSVToRGB(1f * i / result.clusters.Length, 1f, 1f);
+            for (var j = 0; j < result.clusters[i].Length; j++)
+            {
+                var index = result.clusters[i][j];
+                ServiceContainer[index].GetComponent<MeshRenderer>().material.color = color;
+
+                //Gizmos.DrawSphere(data[result.clusters[i][j]], 0.1f);
+                //GizmosUtils.DrawText(GUI.skin, i.ToString(), items[result.clusters[i][j]], Gizmos.color, 10);
+            }
+        }
+
+        clusterized = true;
+    }
+
+    void OnDrawGizmos()
+    {
+        if (running)
+        {
+            return;
+        }
+
+        var items = ServiceContainer.Select(c => c.transform.position).ToArray();
+        var result = KMeans.Cluster(items, ClusterCount, Iterations, 0);
+
+        Gizmos.color = Color.yellow;
+        for (var i = 0; i < result.clusters.Length; i++)
+        {
+            var color = Color.HSVToRGB(1f * i / result.clusters.Length, 1f, 1f);
+            //color.a = 0.5f;
+            Gizmos.color = color;
+            for (var j = 0; j < result.clusters[i].Length; j++)
+            {
+                //Gizmos.DrawSphere(data[result.clusters[i][j]], 0.1f);
+                GizmosUtils.DrawText(GUI.skin, i.ToString(), items[result.clusters[i][j]], Gizmos.color, 10);
+            }
+        }
+    }
+}
+
+public class Service
+{
+    public string Name { get; set; }
+    public List<(int index, int number)> Calling { get; set; }
+    public List<(int index, int number)> CalledBy { get; set; }
+    public List<(int index, int number)> CommonChanges { get; set; }
+}
---
+++ Assets/ServiceContainer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ServiceContainer : MonoBehaviour
+{
+    public string Name;
+
+    public void Init(Service service, List<ServiceContainer> serviceContainer)
+    {
+        Name = service.Name;
+
+        
+
+        foreach (var (index, number) in service.Calling)
+        {
+            AddSpring(index, number, serviceContainer, 1.0f);
+        }
+        foreach (var (index, number) in service.CalledBy)
+        {
+            //AddSpring(index, number, serviceContainer, 1.0f);
+        }
+        foreach (var (index, number) in service.CommonChanges)
+        {
+            //AddSpring(index, number, serviceContainer, 1.0f);
+        }
+    }
+
+    private void AddSpring(int index, int number, List<ServiceContainer> serviceContainer, float factor)
+    {
+        var spring = gameObject.AddComponent<SpringJoint>();
+        spring.autoConfigureConnectedAnchor = false;
+        spring.spring = number * factor;
+        spring.anchor = Vector3.zero;
+        spring.connectedAnchor = Vector3.zero;
+        spring.connectedBody = serviceContainer[index].gameObject.GetComponent<Rigidbody>();
+        spring.enableCollision = true;
+    }
+}
